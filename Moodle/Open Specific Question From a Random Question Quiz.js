7158e9ba7c64acded40559fca992e511fb69327e
Nathan <nathan@abourbih.com>
2019-10-28 16:44:34 -0400

    Update Open Specific Question From a Random Question Quiz.js
    Added a check for if the question was correct or not.

--- Moodle/Open Specific Question From a Random Question Quiz.js
+++ Moodle/Open Specific Question From a Random Question Quiz.js
@@ -3,13 +3,17 @@
 //Change the QuestionName variable below to text that appears on the question popup page.
 //Press F2 and paste the following code into the console window.
 
-var Cambrian_CustomScript_QuestionName = "scalar vs aggregate";
+var Cambrian_CustomScript_QuestionName = "Describe the order of";
 
 $('.grades a[href*="/mod/quiz/reviewquestion.php?"]').each(function () {
+  if ($(this).find('span[class*="partiallycorrect"]').length > 0 || $(this).find('span[class*="incorrect"]').length > 0) {
     $.get(this.href, function (data) {
-        if (data.indexOf(Cambrian_CustomScript_QuestionName) >= 0) {
-            window.open(this.url, '_blank');
-            console.log(this.url);
-        }
+      if (data.indexOf(Cambrian_CustomScript_QuestionName) >= 0) {
+
+        $(this).parent().css('background-color', 'lightgreen');
+        window.open(this.url, '_blank');
+        console.log(this.url);
+      }
     });
+  }
 });
