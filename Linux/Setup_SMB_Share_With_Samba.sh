011bca66868799d34cd56061edd41a2ca8cc01b9
Nathan <nathan@abourbih.com>
2019-12-10 13:38:00 -0500

    Update Setup_SMB_Share_With_Samba.sh

--- Linux/Setup_SMB_Share_With_Samba.sh
+++ Linux/Setup_SMB_Share_With_Samba.sh
@@ -23,25 +23,14 @@ mkdir ~/smb/shares/departments
 mkdir ~/smb/shares/departments/accounting
 mkdir ~/smb/shares/users
 mkdir ~/smb/shares/users/user1
-mkdir ~/smb/shares/users/user1
+mkdir ~/smb/shares/users/user2
 
 #sudo docker stop smbserver				#Optionally stop any existing smbserver docker.
 #sudo docker rm smbserver				#Optionally remove any existing smbserver docker.
 
 #Setup Samba. Replace the variables in this line with appropriate values.
 #The example setup below will create two users, each with their own share. It will also create a read-only public share as well as an accounting share that the two users can access.
-sudo docker run -it --name smbserver \
-			-v ~/smb/shares/public:/shares/public \
-			-v ~/smb/shares/departments/accounting:/shares/departments/accounting \
-			-v ~/smb/shares/users/user1:/shares/users/user1 \
-			-v ~/smb/shares/users/user2:/shares/users/user2 \
-			-p 139:139 -p 445:445 -e TZ=EST5EDT -d dperson/samba -p \
-            -u "user1;supersecret" \
-            -u "user2;extrasecret" \
-            -s "public;/shares/public" \
-            -s "accounting;/shares/departments/accounting;yes;no;no;user1,user2" \
-            -s "user1;/shares/users/user1;yes;no;no;user1" \
-            -s "user2;/shares/users/user2;yes;no;no;user2"
+sudo docker run -it --name smbserver -v ~/smb/shares/public:/shares/public -v ~/smb/shares/departments/accounting:/shares/departments/accounting -v ~/smb/shares/users/user1:/shares/users/user1 -v ~/smb/shares/users/user2:/shares/users/user2 -p 139:139 -p 445:445 -e TZ=EST5EDT -d dperson/samba -p -u "user1;supersecret" -u "user2;extrasecret" -s "public;/shares/public" -s "accounting;/shares/departments/accounting;yes;no;no;user1,user2" -s "user1;/shares/users/user1;yes;no;no;user1" -s "user2;/shares/users/user2;yes;no;no;user2"
 
 #List docker containers.
 sudo docker ps
